756dc73c863fc6133a2f62ee9e4d3add962f9b0b
ShydenMcM <152036300+ShydenMcM@users.noreply.github.com>
2024-12-27 11:07:39 +0000

    End of Section 3 (#3)

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-user_prompt = "Enter a todo: "
-
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo.capitalize())
-    print(todos)
+    user_action = input("Type add, show or exit: ").strip()
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ")
+            todos.append(todo.capitalize())
+        case "show":
+            for item in todos:
+                print(item)
+        case "exit":
+            break
+        case _:
+            print("Invalid command. Please try again.")
 
+print("Goodbye!")
