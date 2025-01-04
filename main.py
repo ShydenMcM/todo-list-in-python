f48ab270b1f70ab07de5da842b889683a148d892
Shyden McM <sasteberis@hotmail.co.uk>
2025-01-04 10:24:07 +0000

    Todo list can now edit items

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ").strip()
+    user_action = input("Type add, show, edit or exit: ").strip()
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
@@ -9,6 +9,12 @@ while True:
         case "show":
             for item in todos:
                 print(item)
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            existing_todo = todos[number]
+            new_todo = input("Enter new todo: ").capitalize()
+            todos[number] = new_todo
         case "exit":
             break
         case _:
