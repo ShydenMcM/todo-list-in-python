87192fe5ccf93410e38c0a37187c4d953f001ee4
ShydenMcM <152036300+ShydenMcM@users.noreply.github.com>
2025-01-05 01:37:14 +0000

    Todo items can now be completed which removes them from the list (#5)

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ").strip()
+    user_action = input("Type add, show, edit, complete or exit: ").strip()
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo.capitalize())
         case "show":
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f"[{index + 1}]-: {item}")
         case "edit":
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             existing_todo = todos[number]
             new_todo = input("Enter new todo: ").capitalize()
             todos[number] = new_todo
+            print(f"Replaced {existing_todo} with {new_todo}")
+        case "complete":
+            number = int(input("Number of the todo to complete: "))
+            print(f"{todos[number-1]} has been completed so it has been removed.")
+            todos.pop(number - 1)
         case "exit":
             break
         case _:
